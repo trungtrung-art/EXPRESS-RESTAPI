8e9fd17e1c322b6ccf0c4e5e3684775c5b065d12
Trần Trung <trantrung@Trans-MacBook-Pro.local>
2025-02-07 09:52:01 +0700

    REST APIs, Clients & CORS Errors

--- app.js
+++ app.js
@@ -8,6 +8,16 @@ const app = express();
 // app.use(bodyParser.urlencoded());
 app.use(bodyParser.json()); // applidation/json
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+        'Access-Control-Allow-Methods',
+        'GET, POST, PUT, PATCH, DELETE'
+    );
+    res.setHeader('Access-Control-Allow-Header', 'Content-Type, Authorization');
+    next();
+});
+
 app.use('/feed', feedRoutes);
 
 app.listen(8080);
